9d4544f267d1e7dc72850a91da75223ff5302208
Katsiara <katsiara@mail.ru>
2023-08-23 20:06:30 +0300

    Виды методов

---
+++ example1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Вид 1
+
+void Method1 ()
+    {
+    Console.WriteLine("Автор...");
+    }
+
+  Method1();  
+
+// Вид2
+void Method2 (string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст");
+
+// Вид3
+int Method3()
+    {
+    return DateTime.Now.Year;
+    }
+ int year = Method3();
+ Console.WriteLine(year);
+
+
+// Вид 4
+
+string Method4 (int count, string c)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i< count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
